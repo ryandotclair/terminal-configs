abc82b1ddc623cde6f5b82ae01b7f36ea8555c29
Ryan Clair <ryandotclair@users.noreply.github.com>
2024-10-21 11:29:35 -0500

    pyenv-virtualenv support

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ curl https://pyenv.run | bash
 cp -r .config ~
 cp .zshrc ~
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+brew install pyenv-virtualenv
