610687aa360cf29f872f7dbec1d922d198f70470
Ryan Clair <ryandotclair@users.noreply.github.com>
2024-10-21 11:23:19 -0500

    pyenv

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-brew install zsh starship zsh-syntax-highlighting jump docker noti
+brew install zsh starship zsh-syntax-highlighting jump docker noti openssl readline sqlite3 xz zlib
 cp -r .config ~
 cp .zshrc ~
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+curl https://pyenv.run | bash
